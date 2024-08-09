b24b0e11ea8e8f4a3f80a051e9078a030b08bd1c
branjevectn <3105729+branjevectn@users.noreply.github.com>
2024-08-09 20:09:28 +0500

    Add files via upload

---
+++ page-content.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Page Content
+ * Slug: travel-agent-blocks/page-content
+ * Categories: travel-agent-blocks, page-content
+ */
+?>
+
+<!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:post-content /--></div>
+<!-- /wp:group -->
