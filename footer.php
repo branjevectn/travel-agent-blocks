4eb9f68c7cec0f3ffc86b55c02983668895beb92
branjevectn <3105729+branjevectn@users.noreply.github.com>
2024-08-09 20:08:07 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Title: Footer
+ * Slug: travel-agent-blocks/footer
+ * Categories: travel-agent-blocks, footer
+ */
+?>
+
+<!-- wp:group {"className":"footer-widgets","style":{"spacing":{"padding":{"top":"60px","right":"20px","bottom":"60px","left":"20px"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"}},"backgroundColor":"heading","textColor":"white","fontSize":"small","layout":{"type":"constrained","contentSize":"80%"}} -->
+<div class="wp-block-group footer-widgets has-white-color has-heading-background-color has-text-color has-background has-small-font-size" style="margin-top:0;margin-bottom:0;padding-top:60px;padding-right:20px;padding-bottom:60px;padding-left:20px"><!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50","left":"var:preset|spacing|20"}}}} -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","width":"23.33%"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:23.33%"><!-- wp:heading {"level":5,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"fontSize":"25px","fontStyle":"normal","fontWeight":"500"}},"textColor":"base"} -->
+<h5 class="wp-block-heading has-base-color has-text-color has-link-color" style="font-size:25px;font-style:normal;font-weight:500"><?php esc_html_e('Quick Links','travel-agent-blocks'); ?></h5>
+<!-- /wp:heading -->
+
+<!-- wp:list {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"500"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base"} -->
+<ul style="font-size:16px;font-style:normal;font-weight:500" class="wp-block-list has-base-color has-text-color has-link-color">
+
+<!-- wp:list-item -->
+<li><a href="#"><?php esc_html_e('Home','travel-agent-blocks'); ?></a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="#"><?php esc_html_e('Services','travel-agent-blocks'); ?></a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="#"><?php esc_html_e('About Us','travel-agent-blocks'); ?></a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="#"><?php esc_html_e('Latest News','travel-agent-blocks'); ?></a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="#"><?php esc_html_e('Privacy Policy','travel-agent-blocks'); ?></a></li>
+<!-- /wp:list-item -->
+
+</ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","width":"23.33%"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:23.33%"><!-- wp:heading {"level":5,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"fontSize":"25px","fontStyle":"normal","fontWeight":"500"}},"textColor":"base"} -->
+<h5 class="wp-block-heading has-base-color has-text-color has-link-color" style="font-size:25px;font-style:normal;font-weight:500"><?php esc_html_e('Support','travel-agent-blocks'); ?></h5>
+<!-- /wp:heading -->
+
+<!-- wp:list {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"500"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base"} -->
+<ul style="font-size:16px;font-style:normal;font-weight:500" class="wp-block-list has-base-color has-text-color has-link-color">
+
+<!-- wp:list-item -->
+<li><a href="#"><?php esc_html_e('Support','travel-agent-blocks'); ?></a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="#"><?php esc_html_e('Contact Us','travel-agent-blocks'); ?></a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="#"><?php esc_html_e('Knowledge Base','travel-agent-blocks'); ?></a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="#"><?php esc_html_e('Partnership','travel-agent-blocks'); ?></a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="#"><?php esc_html_e('FAQ','travel-agent-blocks'); ?></a></li>
+<!-- /wp:list-item -->
+
+</ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","width":"23.33%"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:23.33%"><!-- wp:heading {"level":5,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"fontSize":"25px","fontStyle":"normal","fontWeight":"500"}},"textColor":"base"} -->
+<h5 class="wp-block-heading has-base-color has-text-color has-link-color" style="font-size:25px;font-style:normal;font-weight:500"><?php esc_html_e('Popular Activities','travel-agent-blocks'); ?></h5>
+<!-- /wp:heading -->
+
+<!-- wp:list {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"500"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base"} -->
+<ul style="font-size:16px;font-style:normal;font-weight:500" class="wp-block-list has-base-color has-text-color has-link-color">
+
+<!-- wp:list-item -->
+<li><a href="#"><?php esc_html_e('Hiking Explore','travel-agent-blocks'); ?></a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="#"><?php esc_html_e('Kayaks Explore','travel-agent-blocks'); ?></a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="#"><?php esc_html_e('Fishing Explore','travel-agent-blocks'); ?></a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="#"><?php esc_html_e('Bike Explore','travel-agent-blocks'); ?></a></li>
+<!-- /wp:list-item -->
+
+</ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"30%"} -->
+<div class="wp-block-column" style="flex-basis:30%"><!-- wp:heading {"level":5,"style":{"typography":{"fontSize":"25px","fontStyle":"normal","fontWeight":"500"}},"textColor":"white"} -->
+<h5 class="wp-block-heading has-white-color has-text-color" style="font-size:25px;font-style:normal;font-weight:500"><?php esc_html_e('Get In Touch','travel-agent-blocks'); ?></h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"500"}},"textColor":"base"} -->
+<p class="has-base-color has-text-color has-link-color" style="font-size:16px;font-style:normal;font-weight:500"><?php esc_html_e('Location: 4545 LaJolla Village,California','travel-agent-blocks'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"500"}},"textColor":"base"} -->
+<p class="has-base-color has-text-color has-link-color" style="font-size:16px;font-style:normal;font-weight:500"><?php esc_html_e('Email: info@support.com','travel-agent-blocks'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"500"}},"textColor":"base"} -->
+<p class="has-base-color has-text-color has-link-color" style="font-size:16px;font-style:normal;font-weight:500"><?php esc_html_e('Phone: +1 234 567 8900','travel-agent-blocks'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"className":"app-group","style":{"color":{"background":"#2b2d31"},"spacing":{"padding":{"left":"var:preset|spacing|70","top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","right":"var:preset|spacing|70"}},"border":{"radius":"5px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group app-group has-background" style="border-radius:5px;background-color:#2b2d31;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--70)"><!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"500"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base"} -->
+<p class="has-base-color has-text-color has-link-color" style="font-size:16px;font-style:normal;font-weight:500"><?php esc_html_e('Android App Available','travel-agent-blocks'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"spacing":{"padding":{"bottom":"0","top":"0"},"margin":{"top":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"500"}},"textColor":"primary"} -->
+<p class="has-primary-color has-text-color has-link-color" style="margin-top:0;padding-top:0;padding-bottom:0;font-size:16px;font-style:normal;font-weight:500"><?php esc_html_e('Download the App','travel-agent-blocks'); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"color":{"background":"#2c2d32"}},"layout":{"type":"constrained","contentSize":"80%"}} -->
+<div class="wp-block-group has-background" style="background-color:#2c2d32"><!-- wp:group {"className":"copyright-text","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group copyright-text"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontFamily":"outfit"} -->
+<p class="has-base-color has-text-color has-link-color has-outfit-font-family"><?php esc_html_e('Travel Agent Blocks WordPress Theme','travel-agent-blocks'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"layout":{"selfStretch":"fit","flexSize":null}},"textColor":"base","fontFamily":"outfit"} -->
+<p class="has-base-color has-text-color has-link-color has-outfit-font-family"><?php esc_html_e('Proudly powered by ','travel-agent-blocks'); ?><a rel="noreferrer noopener" href="https://www.ovationthemes.com/" target="_blank"><?php esc_html_e('Ovation Themes','travel-agent-blocks'); ?></a> <?php esc_html_e('and','travel-agent-blocks'); ?> <a href="https://wordpress.org"><?php esc_html_e('WordPress','travel-agent-blocks'); ?></a>.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
