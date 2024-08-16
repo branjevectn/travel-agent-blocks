accb5a9a110f8ecc20dd49c251a602a4131c2bea
Heli. <3105729+heli-diogo@users.noreply.github.com>
2024-08-16 11:50:19 +0500

    Update archive.rs

--- archive.rs
+++ archive.rs
@@ -6,6 +6,6 @@ fn main() {
     a = &b;
     println!("{}",b);
   }  
-// fggfchffvdxjhdguufghydgygsruxdghgydesfggdgydehg
+// fggfchffvdxjhdguufghdfhserydgygsruxdghgydesfggdgydehg
   println!("{}",a);
 }
