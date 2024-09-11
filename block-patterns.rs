93d3c2a0340d3935bf2eaaf1d08d022a35039d45
Heli. <3105729+heli-diogo@users.noreply.github.com>
2024-09-11 10:37:00 +0500

    Update block-patterns.rs

--- block-patterns.rs
+++ block-patterns.rs
@@ -2,7 +2,7 @@ fn helper() -> Box<i32> {
     let three = Box::new(3);
     three // Ownership is transferred implicitly
 }
-// gbddvfghhhghggffdggfxfjhvhh
+// gbddvfghhhgggffdggfxfjhvhh
 fn main() {
     // Acquire ownership of the return value
     let my_three = helper();
